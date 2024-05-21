02d133cefe906c5e81f402d5ceb1fa3e1784f91d
LexaKater <125663812+LexaKater@users.noreply.github.com>
2024-05-21 20:44:33 +0300

    Update Grounded.cs

--- Assets/Scripts/PlayerScripts/Grounded.cs
+++ Assets/Scripts/PlayerScripts/Grounded.cs
@@ -2,21 +2,14 @@ using UnityEngine;
 
 public class Grounded : MonoBehaviour
 {
-    [SerializeField] Transform _player;
+    [SerializeField] private Transform _player;
     [SerializeField, Range(0f, 10f)] private float _rayDistance;
     [SerializeField] private LayerMask _layer;
-
-    private bool _isGrounded;
 
     public bool TryFindGround()
     {
         RaycastHit2D hit = Physics2D.Raycast(_player.position, Vector2.down, _rayDistance, _layer);
 
-        if (hit.collider != null)
-            _isGrounded = true;
-        else
-            _isGrounded = false;
-
-        return _isGrounded;
+        return hit.collider != null;
     }
 }
